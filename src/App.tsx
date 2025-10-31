9e0a37b3a6557bb8c0bc3dfda25782bcb3681794
bry4ng4rrix <bryanmfb4@gmail.com>
2025-10-31 22:21:20 +0300

    <Navbar/>

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
+import Navbar from './components/ui/custom/navbar'
 
 function App() {
-  
+  const [isVar,setisVar] = useState([])
   return (
     <div className='p-2'>
-      hello world
-      <Button className='flex gap-2'>kitio ary </Button>
+      <Navbar/>
     </div>
   )
 }
---
+++ src/components/ui/custom/navbar.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom"
+import { Card } from "../card"
+import { Button } from "../button"
+import { Home , Activity ,Clapperboard , Notebook ,Search , User } from "lucide-react"
+
+type NavItem = {
+    name : string
+}
+
+const Nav:NavItem[] =[
+    {name : 'Home'},
+    {name : 'Activity'},
+    {name : 'Spot'},
+    {name : 'A propos'},
+]
+
+const navbar = () => {
+  return (
+    <div className=" flex justify-between">
+      <Card className="bg-foreground text-white p-2 px-3 rounded-full flex  space-x-6">
+       { Nav.map((n ,index)=> (
+            <div key={index} className="rounded-full  px-2">{n.name}</div>
+       ))}
+      </Card>
+      <div className="flex space-x-6">
+        <Button className="rounded-full">Contact</Button>
+            <div className="flex gap-2">
+                <Button className="rounded-full" variant='outline'><Search/></Button>
+                <Button className=" bg-foreground text-white rounded-full" variant='outline'><User/></Button>
+            </div>
+      </div>
+    </div>
+  )
+}
+
+export default navbar
