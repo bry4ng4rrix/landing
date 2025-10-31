885761238b0fe564659109a3fc27438ee8c7660a
bry4ng4rrix <bryanmfb4@gmail.com>
2025-10-31 12:09:08 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Button } from './components/ui/button'
 
 function App() {
   
   return (
-    <div className='h-screen'>
+    <div className=''>
       hello world
+      <Button className='flex gap-2'>kitio ary </Button>
     </div>
   )
 }
